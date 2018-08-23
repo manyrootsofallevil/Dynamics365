276fa3e15073a3e121090d6779c53c3055f89da1
manyrootsofallevil <manyrootsofallevil@gmail.com>
2018-08-23 21:03:52 +0100

    getting metadata now

--- Plugins/Plugins/Account.cs
+++ Plugins/Plugins/Account.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Metadata;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +18,11 @@ namespace Plugins
 
             IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
 
+            var orgFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            var orgService = orgFactory.CreateOrganizationService(context.UserId);
+
+
+
             if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
             {
                 var account = (Entity)context.InputParameters["Target"];
@@ -23,8 +30,11 @@ namespace Plugins
                 if (account.LogicalName != "account") { return; }
 
                 try
-                {                    
-                    account["new_searchengine"] = new OptionSetValue(GetSearchEngineOptionSet(account["name"].ToString()));
+                {
+                    var searchEngines = GetSearchEngines(orgService);
+
+                    account["new_searchengine"] =
+                        new OptionSetValue(GetSearchEngineOptionSet(account["name"].ToString(), searchEngines));
                 }
 
                 catch (FaultException<OrganizationServiceFault> ex)
@@ -40,25 +50,52 @@ namespace Plugins
             }
 
         }
-     
 
-        protected int GetSearchEngineOptionSet(string accountName)
+        protected Dictionary<int, string> GetSearchEngines(IOrganizationService orgService)
+        {
+            var options = new Dictionary<int, string>();
+
+            var attributeRequest = new RetrieveAttributeRequest()
+            {
+                EntityLogicalName = "account",
+                LogicalName = "new_searchengine",
+                RetrieveAsIfPublished = true
+            };
+
+            var attributeResponse = (RetrieveAttributeResponse)orgService.Execute(attributeRequest);
+            var attributeMetadata = (EnumAttributeMetadata)attributeResponse.AttributeMetadata;
+
+            options = attributeMetadata.OptionSet.Options
+               .Select(option => new
+               {
+                   Value = option.Value.HasValue ? (int)option.Value : 0,
+                   Text = option.Label.UserLocalizedLabel.Label
+               })
+               .ToDictionary(x => x.Value, x => x.Text);
+
+            return options;
+        }
+
+        protected int GetSearchEngineOptionSet(string accountName, Dictionary<int, string> searchEngines)
         {
             int output = 0;
 
-            Dictionary<int, string> searchEngines = new Dictionary<int, string>();
-            searchEngines.Add(100000000, "bing.com");
-            searchEngines.Add(100000001, "dogpile.com");
-            searchEngines.Add(100000002, "duckduckgo.com");
-            searchEngines.Add(100000003, "google.com");
-            searchEngines.Add(100000004, "yippy.com");
+
+
+
+            //Dictionary<int, string> searchEngines = new Dictionary<int, string>();
+            //searchEngines.Add(100000000, "bing.com");
+            //searchEngines.Add(100000001, "dogpile.com");
+            //searchEngines.Add(100000002, "duckduckgo.com");
+            //searchEngines.Add(100000003, "google.com");
+            //searchEngines.Add(100000004, "yippy.com");
 
             //Let's see if it there is a first letter match with any of our approved search engines.
             var searchEngine = searchEngines
                 .Where(x => x.Value.ToLower().Substring(0, 1) == accountName.ToLower()[0].ToString())
                 .FirstOrDefault();
 
-            if (!searchEngine.Equals(default(KeyValuePair<int,string>)))
+            if (!searchEngine.Equals(default(KeyValuePair<int, string>)))
             {
                 output = searchEngine.Key;
             }
--- Plugins/PluginsTest/AccountTest.cs
+++ Plugins/PluginsTest/AccountTest.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using NUnit;
+﻿using System.Collections.Generic;
 using NUnit.Framework;
 using Plugins;
 
@@ -11,16 +10,24 @@ namespace PluginsTest
         [TestCase("Ontological", 0)]
         [TestCase("Bastion", 100000000)]
         [TestCase("bastion", 100000000)]
-        [TestCase("Distress",100000001 )]
+        [TestCase("Distress", 100000001)]
         [TestCase("diamon", 100000001)]
         [TestCase("Yahoo", 100000004)]
         [TestCase("yep", 100000004)]
         public void GetSearchEngineOptionSetTest(string accountName, int expectedSearchEngine)
         {
             //Arrange
+            var searchEngines = new Dictionary<int, string>()
+            {
+                {100000000, "bing.com"},
+                {100000001, "dogpile.com"},
+                {100000002, "duckduckgo.com"},
+                {100000003, "google.com"},
+                {100000004, "yippy.com"},
+            };
 
             //Act
-            var searchEngine = GetSearchEngineOptionSet(accountName);
+            var searchEngine = GetSearchEngineOptionSet(accountName, searchEngines);
             //Assert
             Assert.AreEqual(expectedSearchEngine, searchEngine);
         }
