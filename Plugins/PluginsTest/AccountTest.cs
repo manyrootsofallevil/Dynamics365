3047eac0fb7361dd531ef188d911d1a72d040efe
manyrootsofallevil <manyrootsofallevil@gmail.com>
2018-08-24 17:42:22 +0100

    reverted

--- Plugins/PluginsTest/AccountTest.cs
+++ Plugins/PluginsTest/AccountTest.cs
@@ -1,19 +1,17 @@
-﻿using System.Collections.Generic;
-using Microsoft.Xrm.Sdk;
-using Microsoft.Xrm.Sdk.Messages;
-using Moq;
+﻿using System;
+using NUnit;
 using NUnit.Framework;
 using Plugins;
 
 namespace PluginsTest
 {
-    public class AccountTest
+    public class AccountTest : Account
     {
 
         [TestCase("Ontological", 0)]
         [TestCase("Bastion", 100000000)]
         [TestCase("bastion", 100000000)]
-        [TestCase("Distress", 100000001)]
+        [TestCase("Distress",100000001 )]
         [TestCase("diamon", 100000001)]
         [TestCase("Yahoo", 100000004)]
         [TestCase("yep", 100000004)]
@@ -21,15 +19,8 @@ namespace PluginsTest
         {
             //Arrange
 
-            Mock<IOrganizationService> orgService = new Mock<IOrganizationService>();
-
-            orgService.Setup(org => org.Execute(new RetrieveAttributeRequest()))
-                .Returns(new RetrieveAttributeResponse() { Results = new ParameterCollection() });
-            
-            SearchEngineChecker sut = new SearchEngineChecker();
-
             //Act
-            var searchEngine = sut.GetSearchEngineOptionSet(accountName, orgService.Object);
+            var searchEngine = GetSearchEngineOptionSet(accountName);
             //Assert
             Assert.AreEqual(expectedSearchEngine, searchEngine);
         }
