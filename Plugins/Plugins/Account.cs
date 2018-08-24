6a8fedf37c23f3dfb065a7a65aaab14610c80eb8
manyrootsofallevil <manyrootsofallevil@gmail.com>
2018-08-24 19:00:24 +0100

    PArt 3 - 1 (not finished)

--- Plugins/Plugins/Account.cs
+++ Plugins/Plugins/Account.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +17,9 @@ namespace Plugins
 
             IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
 
+            var organizationFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            var orgService = organizationFactory.CreateOrganizationService(context.UserId);
+
             if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
             {
                 var account = (Entity)context.InputParameters["Target"];
@@ -23,8 +27,10 @@ namespace Plugins
                 if (account.LogicalName != "account") { return; }
 
                 try
-                {                    
-                    account["new_searchengine"] = new OptionSetValue(GetSearchEngineOptionSet(account["name"].ToString()));
+                {
+                    var searchEngine = GetSearchEngine(account["name"].ToString(), orgService, tracingService);
+
+                    if (searchEngine != null) { account["new_searchengineid"] = searchEngine; }
                 }
 
                 catch (FaultException<OrganizationServiceFault> ex)
@@ -40,7 +46,39 @@ namespace Plugins
             }
 
         }
-     
+
+        private EntityReference GetSearchEngine(string accountName, IOrganizationService service)
+        {
+            EntityReference output = null;
+
+            var query = $@"<fetch version='1.0'  mapping='logical' distinct='true'>
+  <entity name='new_searchengine'>
+    <attribute name='new_searchengineid' />
+    <attribute name='new_name' />  
+    <order attribute='new_name' descending='false' />
+    <filter type='and'>
+      <condition attribute='new_name' operator='like' value = '{accountName[0]}%' />
+    </filter>
+  </entity>
+</fetch>";
+
+          
+            var searchEngines = service.RetrieveMultiple(new FetchExpression(query))?.Entities;        
+
+            var searchEngine = searchEngines
+               .Where(se => se["new_name"].ToString().ToLower().Substring(0, 1) == accountName.ToLower()[0].ToString())
+               .FirstOrDefault();
+
+            if (searchEngine != null)
+            {
+                output = new EntityReference("new_searchengine", new Guid(searchEngine["new_searchengineid"].ToString()));
+            }
+
+            return output;
+
+
+        }
+
 
         protected int GetSearchEngineOptionSet(string accountName)
         {
@@ -58,7 +96,7 @@ namespace Plugins
                 .Where(x => x.Value.ToLower().Substring(0, 1) == accountName.ToLower()[0].ToString())
                 .FirstOrDefault();
 
-            if (!searchEngine.Equals(default(KeyValuePair<int,string>)))
+            if (!searchEngine.Equals(default(KeyValuePair<int, string>)))
             {
                 output = searchEngine.Key;
             }
