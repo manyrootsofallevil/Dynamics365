b4c0e7a6eeb9755c0a362d1eb22bda476ab1b9e7
manyrootsofallevil <manyrootsofallevil@gmail.com>
2018-09-03 18:28:38 +0100

    adding a webresource

---
+++ Plugins/WebResources/Account/account.ts
@@ -0,0 +1,14 @@
+﻿/// <reference path="../node_modules/@types/xrm/index.d.ts" />
+
+namespace webresources {
+
+    export class Account {
+
+        static getUrl() {
+
+            var context = Xrm.Utility.getGlobalContext();
+
+            alert("Greetings User");
+        }
+    }
+}
